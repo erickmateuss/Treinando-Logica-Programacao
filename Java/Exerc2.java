677a9a03535f9d61a58c58c7e20aa4da35178841
Erick <140833560+erickmateuss@users.noreply.github.com>
2024-08-29 21:50:35 -0300

    fazendo alterações e fazendo o 2 e 3 exercício

--- Java/Exerc2.java
+++ Java/Exerc2.java
@@ -1,3 +1,29 @@
 //Escrever um programa para determinar o consumo médio de um automóvel, 
 //sendo fornecida a distância total percorrida pelo automóvel e o total de combustível gasto. 
 
+package aula01;
+
+import java.util.*;
+
+public  class Exerc2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Quantos litros foram abastecidos: ");
+        int litrosAbastecidos = sc.nextInt();
+
+        System.out.println("Quantos quilometros foram rodados: ");
+        int quilometroRodados = sc.nextInt();
+
+        double consumoMedio = (double) litrosAbastecidos / quilometroRodados;
+
+
+
+        System.out.printf("O consumo médio do automóvel é %.2f km/l1%n", consumoMedio);
+
+
+
+    }
+}
--- Java/Exerc3.java
+++ Java/Exerc3.java
@@ -1,4 +1,22 @@
 // Escreva um programa que entre com um
 // n�mero e o imprima caso seja maior do que 20.
 
+package aula01;
 
+import java.util.Scanner;
+
+public class Exerc3 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite um número: ");
+        int numero = sc.nextInt();
+
+        if (numero >= 20) {
+            System.out.println("O número " + numero + " é maior que 20!");
+        }else
+            System.out.println("O número " + numero + " é menor que 20!");
+    }
+
+}
